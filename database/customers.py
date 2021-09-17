d6a1cbe335e0c6079e315c377b7d7f6b4484ac0b
Leonardo Felipe dos Santos <42083917+leosan95@users.noreply.github.com>
2021-09-17 13:16:38 -0300

    Separação logins em credentials.py

--- database/customers.py
+++ database/customers.py
@@ -1,5 +1,6 @@
 from getpass import getpass
 from mysql.connector import connect, Error
+import credentials
 
 CreateDB = '''CREATE TABLE CUSTOMERS(
                 id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -13,24 +14,31 @@ CreateDB = '''CREATE TABLE CUSTOMERS(
                 CPF_CNPJ VARCHAR(75) NOT NULL, 
                 Payment VARCHAR(500),
                 NOTES TEXT);'''
-CreateTesteDB = '''INSERT INTO CUSTOMERS (Name, NumberUC, Localization, CEP, District, Email, Phone, CPF_CNPJ, Payment, NOTES) 
-                VALUES ('Leonardo Felipe', '000001', 'Rua Daltro Filho', '98801-510', 'Centro', 'Leonardo@teste', '(55)99992-1300', '000.000.000-01', 'Entrada: 5000, Finalização: 3000', 'TEXTO DE OBSERVAÇÕES.')'''
 
-def ConectBD():
-        try:
-                with connect(
-                        host="localhost",
-                        user="test",
-                        password='12345678',
-                        database='ordemservicosenergensteste',
-                ) as connection:
-                        with connection.cursor() as cursor:
-                                cursor.execute(CreateDB)
-                                cursor.execute(CreateTesteDB)
-                                connection.commit()
-        except Error as e:
-                print(e)
-
+def CreateDB():
+        connection = mysql.connector.connect(
+                host= credentials.host,
+                user= credentials.user,
+                password= credentials.password,
+                database= credentials.database
+        )
+        cursor = connection.cursor()
+        sql = '''CREATE TABLE CUSTOMERS(
+                        id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                        Name TEXT NOT NULL,
+                        NumberUC VARCHAR(255) NOT NULL,
+                        Localization VARCHAR(500) NOT NULL,
+                        CEP VARCHAR(40) NOT NULL,
+                        District VARCHAR(500) NOT NULL, 
+                        Email VARCHAR(255) NOT NULL,
+                        Phone VARCHAR(50) NOT NULL,
+                        CPF_CNPJ VARCHAR(75) NOT NULL, 
+                        Payment VARCHAR(500),
+                        NOTES TEXT);'''
+        cursor.execute(sql)
+        connection.commit()
+        cursor.close()
+        connection.close()
 
 
 ## Name, NumberUC, Localization, CEP, District, Email, Phone, CPF_CNPJ, Payment, NOTES
